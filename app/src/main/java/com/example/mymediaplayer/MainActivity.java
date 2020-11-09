fd5f7a87cb186e68867eedc35b29610943fe0a92
vprokhorova28 <vpro2004@mail.ru>
2020-11-09 17:17:21 +0300

    Version 1.2

--- app/src/main/java/com/example/mymediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mymediaplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -17,6 +18,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mp = MediaPlayer.create(this, R.raw.song);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this,"start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"pause", Toast.LENGTH_SHORT).show();
+        mp.pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"destroy", Toast.LENGTH_SHORT).show();
+        mp.stop();
+        try {
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"stop", Toast.LENGTH_SHORT).show();
+        mp.pause();
     }
 
     public void onClick(View view) {
